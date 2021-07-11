feb65130ed05a51584b3e2ef6ebb99c4b1556abc
Vagner Silveira de Oliveira <vagner.olliver@yahoo.com.br>
2021-07-11 12:50:46 -0300

    chore: challange done

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,25 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+      if(!newTaskTitle) return
+      setTasks([...tasks, { 
+        id: Math.floor(Math.random() * 1000),
+        title: newTaskTitle,
+        isComplete: false
+      }])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const results = tasks.map((item: Task) => { 
+      if (item.id !== id) { return item }
+      else { return { ...item, isComplete: !item.isComplete } } 
+    }) as Task[];
+ 
+    setTasks(results)
   }
-
+  
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((item: Task) => item.id !== id ))
   }
 
   return (
